68a7dcc2d8fd2876c9c29e9d232e9f84d1992b67
Adam Grare <agrare@redhat.com>
2019-03-14 12:55:22 -0400

    Review comments

--- lib/sources/api/events.rb
+++ lib/sources/api/events.rb
@@ -1,27 +1,23 @@
 module Sources
   module Api
-    class Events
-      class << self
-        def raise_event(event, payload)
-          messaging_client.publish_topic(
-            :service => "platform.sources.event-stream",
-            :event   => event,
-            :payload => payload
-          )
-        end
+    module Events
+      def self.raise_event(event, payload)
+        messaging_client.publish_topic(
+          :service => "platform.sources.event-stream",
+          :event   => event,
+          :payload => payload
+        )
+      end
 
-        private
+      private_class_method def self.messaging_client
+        require "manageiq-messaging"
 
-        def messaging_client
-          require "manageiq-messaging"
-
-          @messaging_client ||= ManageIQ::Messaging::Client.open({
-            :protocol => :Kafka,
-            :host     => ENV["QUEUE_HOST"] || "localhost",
-            :port     => ENV["QUEUE_PORT"] || "9092",
-            :encoding => "json"
-          })
-        end
+        @messaging_client ||= ManageIQ::Messaging::Client.open(
+          :protocol => :Kafka,
+          :host     => ENV["QUEUE_HOST"] || "localhost",
+          :port     => ENV["QUEUE_PORT"] || "9092",
+          :encoding => "json"
+        )
       end
     end
   end
