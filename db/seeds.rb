2038734489cac3f29428b69360b6d3eec6a0e7e2
Adam Grare <agrare@redhat.com>
2019-03-20 11:01:21 +0100

    Update source type seeds with password fields.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ openshift_json_schema = {
     {:component => "text-field", :name => "url", :label => "URL"},
     {:component => "checkbox", :name => "verify_ssl", :label => "Verify SSL"},
     {:component => "text-field", :name => "certificate_authority", :label => "Certificate Authority", :condition => {:when => "verify_ssl", :is => true}},
-    {:component => "textarea-field", :name => "token", :label => "Token"}
+    {:component => "text-field", :name => "token", :label => "Token", :type => "password"}
   ]
 }
 update_or_create(SourceType, :name => "openshift", :product_name => "OpenShift", :vendor => "Red Hat", :schema => openshift_json_schema)
@@ -22,7 +22,7 @@ amazon_json_schema = {
   :title  => "Configure AWS",
   :fields => [
     {:component => "text-field", :name => "user_name", :label => "Access Key"},
-    {:component => "text-field", :name => "password", :label => "Secret Key"}
+    {:component => "text-field", :name => "password", :label => "Secret Key", :type => "password"}
   ]
 }
 update_or_create(SourceType, :name => "amazon", :product_name => "AWS", :vendor => "Amazon", :schema => amazon_json_schema)
@@ -34,7 +34,7 @@ ansible_tower_json_schema = {
     {:component => "checkbox", :name => "verify_ssl", :label => "Verify SSL"},
     {:component => "text-field", :name => "certificate_authority", :label => "Certificate Authority", :condition => {:when => "verify_ssl", :is => true}},
     {:component => "text-field", :name => "user", :label => "User name"},
-    {:component => "text-field", :name => "password", :label => "Secret Key"}
+    {:component => "text-field", :name => "password", :label => "Secret Key", :type => "password"}
   ]
 }
 update_or_create(SourceType, :name => "ansible-tower", :product_name => "Ansible Tower", :vendor => "Red Hat", :schema => ansible_tower_json_schema)
