e50a4e0043b8df5640031c68e6edfdc9918cf442
Nick Carboni <ncarboni@redhat.com>
2019-04-05 11:07:16 -0400

    Use the routing helper from api common for major version redirects
    This changes the redirect code from 301 to 302 and fixes an issue
    where /openapi.json would redirect to /openapi

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   # Disable PUT for now since rails sends these :update and they aren't really the same thing.
   def put(*_args); end
 
+  routing_helper = ManageIQ::API::Common::Routing.new(self)
+
   prefix = "api"
   if ENV["PATH_PREFIX"].present? && ENV["APP_NAME"].present?
     prefix = File.join(ENV["PATH_PREFIX"], ENV["APP_NAME"]).gsub(/^\/+|\/+$/, "")
   end
 
   scope :as => :api, :module => "api", :path => prefix do
-    match "/v0/*path", :via => [:delete, :get, :options, :patch, :post], :to => redirect(:path => "/#{prefix}/v0.1/%{path}", :only_path => true)
+    routing_helper.redirect_major_version("v0.1", prefix)
 
     namespace :v0x1, :path => "v0.1" do
       get "/openapi.json", :to => "root#openapi"
@@ -28,7 +30,7 @@ Rails.application.routes.draw do
   end
 
   scope :as => :internal, :module => "internal", :path => "internal" do
-    match "/v0/*path", :via => [:get], :to => redirect(:path => "/internal/v0.0/%{path}", :only_path => true)
+    routing_helper.redirect_major_version("v0.0", "internal", :via => [:get])
 
     namespace :v0x1, :path => "v0.1" do
       resources :authentications, :only => [:show]
