172c3102416aa070cea0ad7143c71f843944f5b7
Alberto Bellotti <abellott@redhat.com>
2019-04-16 17:18:56 -0400

    Adding some spec tests to verify that filtering is enabled. - Added a /api/v0.1/ filter request specs - Updated the paginated_response_matcher like the topological inventory api   as filters could be in the paging links so offset may be ? or & delimited.

--- spec/support/paginated_response_matcher.rb
+++ spec/support/paginated_response_matcher.rb
@@ -6,8 +6,8 @@ def paginated_response(count, data)
       "offset" => kind_of(Integer)
     },
     "links" => a_hash_including(
-      "first" => a_string_including("?offset=0"),
-      "last"  => a_string_including("?offset=")
+      "first" => a_string_including("offset=0"),
+      "last"  => a_string_including("offset=")
     ),
     "data"  => data
   }
