e12750331fc1de9a793fdcabdf4992ec587bccb0
Alberto Bellotti <abellott@redhat.com>
2019-04-15 20:18:50 -0400

    Updated Sources-API to OpenAPI 3.0.0 specification - public/doc/openapi-3-v0.1.0.json - bundle exec rake openapi:generate
    Now Leveraging OpenAPI library from manageiq-api-common
    
    This is a prerequisite to enabling filtering with the sources api.

--- lib/api/docs.rb
+++ lib/api/docs.rb
@@ -1,3 +1,3 @@
 module Api
-  Docs = ::OpenApi::Docs.new(Dir.glob(Rails.root.join("public/doc/swagger*.yaml")))
+  Docs = ::ManageIQ::API::Common::OpenApi::Docs.new(Dir.glob(Rails.root.join("public", "doc", "openapi*.json")))
 end
