0a03c1b3953b18079c8ba3db8475d1d654daf77e
Adam Grare <agrare@redhat.com>
2019-04-22 09:40:05 -0400

    Add "role" to source_type seeds.
    Extracted from https://github.com/ManageIQ/topological_inventory-core/pull/125

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ end
 openshift_json_schema = {
   :title  => "Configure OpenShift",
   :fields => [
+    {:component => "text-field", :name => "role", :type => "hidden", :initialValue => "kubernetes"},
     {:component => "text-field", :name => "url", :label => "URL"},
     {:component => "checkbox", :name => "verify_ssl", :label => "Verify SSL"},
     {:component => "text-field", :name => "certificate_authority", :label => "Certificate Authority", :condition => {:when => "verify_ssl", :is => true}},
@@ -21,6 +22,7 @@ update_or_create(SourceType, :name => "openshift", :product_name => "OpenShift",
 amazon_json_schema = {
   :title  => "Configure AWS",
   :fields => [
+    {:component => "text-field", :name => "role", :type => "hidden", :initialValue => "aws"},
     {:component => "text-field", :name => "user_name", :label => "Access Key"},
     {:component => "text-field", :name => "password", :label => "Secret Key", :type => "password"}
   ]
@@ -30,6 +32,7 @@ update_or_create(SourceType, :name => "amazon", :product_name => "AWS", :vendor 
 ansible_tower_json_schema = {
   :title  => "Configure AnsibleTower",
   :fields => [
+    {:component => "text-field", :name => "role", :type => "hidden", :initialValue => "ansible"}, # FIXME: Find the correct value.
     {:component => "text-field", :name => "url", :label => "URL"},
     {:component => "checkbox", :name => "verify_ssl", :label => "Verify SSL"},
     {:component => "text-field", :name => "certificate_authority", :label => "Certificate Authority", :condition => {:when => "verify_ssl", :is => true}},
