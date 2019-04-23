22bd37eed478d4cee8d05d2e5cbf5584d9559112
Adam Grare <agrare@redhat.com>
2019-04-23 09:20:14 -0400

    Don't need to use the extra tenant parameter
    Just pull the tenant from the identity headers

--- app/controllers/api/v1/sources_controller.rb
+++ app/controllers/api/v1/sources_controller.rb
@@ -7,11 +7,7 @@ module Api
       include Api::V1::Mixins::UpdateMixin
 
       def create
-        # TODO does this need a transaction
-        tenant_id     = Tenant.find_or_create_by!(:external_tenant => params_for_create.fetch("tenant")).id
-        create_params = params_for_create.except("tenant").merge("uid" => SecureRandom.uuid, "tenant_id" => tenant_id)
-
-        source = Source.create!(create_params)
+        source = Source.create!(params_for_create.merge("uid" => SecureRandom.uuid))
 
         Sources::Api::Events.raise_event("#{model}.create", source.as_json)
 
