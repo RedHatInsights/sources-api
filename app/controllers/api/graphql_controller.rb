03aca06f5a0455859c669bdc76245c8c9b88f57e
Alberto Bellotti <abellott@redhat.com>
2019-06-06 11:03:57 -0400

    Fixing issue with the tenant field returning the object instead of the serialized external tenant.
    Leveraging the field resolvers option of the schema overlay
    for this capability.

--- app/controllers/api/graphql_controller.rb
+++ app/controllers/api/graphql_controller.rb
@@ -3,7 +3,16 @@ require "manageiq/api/common/graphql"
 module Api
   class GraphqlController < ApplicationController
     def query
-      graphql_api_schema = ::ManageIQ::API::Common::GraphQL::Generator.init_schema(request)
+      schema_overlay = {
+        "^.+$" => {
+          "field_resolvers" => {
+            "tenant" => <<-RESOLVER
+              { obj.tenant.external_tenant }
+            RESOLVER
+          }
+        }
+      }
+      graphql_api_schema = ::ManageIQ::API::Common::GraphQL::Generator.init_schema(request, schema_overlay)
       variables = ::ManageIQ::API::Common::GraphQL.ensure_hash(params[:variables])
       result = graphql_api_schema.execute(
         params[:query],
