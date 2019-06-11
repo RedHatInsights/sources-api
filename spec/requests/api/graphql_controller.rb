c2533f13a236ca204bd2148aa5d74db41ae719fb
Alberto Bellotti <abellott@redhat.com>
2019-06-11 18:55:57 -0400

    Updated the GraphQL specs to reflect the simplified query signature.

--- spec/requests/api/graphql_controller.rb
+++ spec/requests/api/graphql_controller.rb
@@ -3,10 +3,10 @@ RSpec.describe("v1.0 - GraphQL") do
   let!(:source_type) { SourceType.create!(:name => "SourceType", :vendor => "Some Vendor", :product_name => "Product Name") }
   let!(:source)      { Source.create!(:tenant_id => tenant.id, :name => "sample_source", :source_type => source_type, :uid => "123") }
 
-  let!(:graphql_source_query) { { "query" => "{ sources(id: \"#{source.id}\") { edges { node { tenant } } } }" }.to_json }
+  let!(:graphql_source_query) { { "query" => "{ sources(id: \"#{source.id}\") { tenant } }" }.to_json }
 
   def result_source_tenant(response_body)
-    JSON.parse(response_body).fetch_path("data", "sources", "edges").collect { |edge| edge.fetch_path("node", "tenant") }
+    JSON.parse(response_body).fetch_path("data", "sources").collect { |source| source["tenant"] }
   end
 
   context "querying source tenant" do
