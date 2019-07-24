f5bd690a404f3a8a6460bd9d3ace65f2ae144455
Alberto Bellotti <abellott@redhat.com>
2019-07-24 16:29:25 -0400

    Fixed n+1 during sources' endpoint status checks.

--- lib/source_availability_checker.rb
+++ lib/source_availability_checker.rb
@@ -37,7 +37,7 @@ class SourceAvailabilityChecker
   def check_available_sources
     check_sources = []
 
-    Source.joins(:endpoints).each do |source|
+    Source.includes(:endpoints).each do |source|
       next unless source_available(source)
 
       check_sources << source.id
@@ -61,7 +61,7 @@ class SourceAvailabilityChecker
   def check_unavailable_sources
     check_sources = []
 
-    Source.joins(:endpoints).each do |source|
+    Source.includes(:endpoints).each do |source|
       next if source_available(source)
 
       check_sources << source.id
