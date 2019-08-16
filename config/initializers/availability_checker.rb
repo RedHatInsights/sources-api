43cf647777659d0a19e991a9996317be96e73bf1
Alberto Bellotti <abellott@redhat.com>
2019-08-16 10:09:30 -0400

    Removing the availability checker and related threads from the sources-api.  This will now be handled externally via a worker and triggered via OpenShift in the sources-monitor project.
    This PR now just implements the availability_status attribute, JSON and specs.

--- config/initializers/availability_checker.rb
+++
@@ -1,7 +0,0 @@
-# Instantiate the Source Availability Checker Singleton
-Rails.application.config.after_initialize do
-  if defined?(::Rails::Server)
-    SourceAvailabilityChecker.instance
-    puts "Source Availability Checker started ..."
-  end
-end
