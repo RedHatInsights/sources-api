e7fc847a40d442a20e106efa97f6a5ed9d3d8b11
Adam Grare <agrare@redhat.com>
2019-08-30 09:57:45 -0400

    Use Pathname join instead of File.join

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 # Run all seed files in db/seeds/*.rb
-Dir[File.join(Rails.root.join("db", "seeds"), "**/*.rb")].each { |f| load f }
+Dir[Rails.root.join("db", "seeds", "**", "*.rb")].each { |f| load f }
