e6d1a7d4e53443741ac7ab78f469c10178460d1a
Adam Grare <agrare@redhat.com>
2019-08-30 09:59:12 -0400

    Prefer require over load

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 # Run all seed files in db/seeds/*.rb
-Dir[Rails.root.join("db", "seeds", "**", "*.rb")].each { |f| load f }
+Dir[Rails.root.join("db", "seeds", "**", "*.rb")].each { |f| require f }
