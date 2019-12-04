9b479341fd272b5427e1ba1a5208a4e56c28832f
Alberto Bellotti <abellott@redhat.com>
2019-12-04 17:48:48 -0500

    Adding support for application availability checks.
    - Now triggering application availability checks with
      the source check_availability action.

--- app/controllers/api/v1/sources_controller.rb
+++ app/controllers/api/v1/sources_controller.rb
@@ -1,3 +1,6 @@
+require "base64"
+require "net/http"
+
 module Api
   module V1
     class SourcesController < ApplicationController
@@ -30,7 +33,33 @@ module Api
           }
         )
 
+        check_application_availability(source)
+
         render :json => {}, :status => :accepted
+      end
+
+      private
+
+      def check_application_availability(source)
+        source.application_types.each do |app_type|
+          url = app_type.availability_check_url
+          next if url.blank?
+
+          headers = {
+            "Content-Type"  => "application/json",
+            "x-rh-identity" => Base64.strict_encode64({'identity' => { 'account_number' => source.tenant.external_tenant }}.to_json)
+          }
+
+          uri = URI.parse(url)
+          net_http = Net::HTTP.new(uri.host, uri.port)
+          request  = Net::HTTP::Post.new(uri.request_uri, headers)
+          request.body = { "source_id" => source.id.to_s }.to_json
+
+          response = net_http.request(request)
+          next if response.kind_of?(Net::HTTPSuccess)
+
+          logger.info("Failed to request application availability check for source #{source.id} @ #{url} - #{response.message}")
+        end
       end
     end
   end
