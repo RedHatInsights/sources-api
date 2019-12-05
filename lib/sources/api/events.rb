59a228bcb257f585608fe87b6f2a74b8a9613cff
Alberto Bellotti <abellott@redhat.com>
2019-12-05 15:15:44 -0500

    Updated Sources::Api::Events to drop its messaging_client and use the Sources::Api::Messaging.client so we're sharing the same with the activity checker.

--- lib/sources/api/events.rb
+++ lib/sources/api/events.rb
@@ -12,18 +12,7 @@ module Sources
 
         publish_opts[:headers] = headers if headers
 
-        messaging_client.publish_topic(publish_opts)
-      end
-
-      private_class_method def self.messaging_client
-        require "manageiq-messaging"
-
-        @messaging_client ||= ManageIQ::Messaging::Client.open(
-          :protocol => :Kafka,
-          :host     => ENV["QUEUE_HOST"] || "localhost",
-          :port     => ENV["QUEUE_PORT"] || "9092",
-          :encoding => "json"
-        )
+        Messaging.client.publish_topic(publish_opts)
       end
     end
   end
