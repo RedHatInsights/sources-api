b5f5bb36e422333d63608afe62c8fa32e1f9c9be
Adam Grare <agrare@redhat.com>
2020-01-24 13:48:19 -0500

    Check list of topics before publishing
    Check the list of kafka topics before publishing a message to prevent
    invalid messages from blocking others

--- lib/sources/api/messaging.rb
+++ lib/sources/api/messaging.rb
@@ -1,16 +1,22 @@
 module Sources
   module Api
     module Messaging
-      def self.client(topic)
+      def self.client
         require "manageiq-messaging"
 
-        @client ||= {}
-        @client[topic] ||= ManageIQ::Messaging::Client.open(
-          :protocol => :Kafka,
-          :host     => ENV["QUEUE_HOST"] || "localhost",
-          :port     => ENV["QUEUE_PORT"] || "9092",
-          :encoding => "json"
-        )
+        Thread.current[:messaging_client] ||= begin
+          ManageIQ::Messaging::Client.open(
+            :protocol => :Kafka,
+            :host     => ENV["QUEUE_HOST"] || "localhost",
+            :port     => ENV["QUEUE_PORT"] || "9092",
+            :encoding => "json"
+          )
+        end
+      end
+
+      def self.topics
+        # TODO add an interface to ManageIQ::Messaging::Client to get a topic list
+        client.send(:kafka_client)&.topics || []
       end
     end
   end
--- spec/requests/api/v1.0/sources_spec.rb
+++ spec/requests/api/v1.0/sources_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe("v1.0 - Sources") do
   let(:client)          { instance_double("ManageIQ::Messaging::Client") }
   before do
     allow(client).to receive(:publish_topic)
-    allow(Sources::Api::Events).to receive(:messaging_client).and_return(client)
+    allow(Sources::Api::Messaging).to receive(:client).and_return(client)
   end
 
   describe("/api/v1.0/sources") do
@@ -323,6 +323,13 @@ RSpec.describe("v1.0 - Sources") do
     before do
       allow(messaging_client).to receive(:publish_topic)
       allow(Sources::Api::Messaging).to receive(:client).and_return(messaging_client)
+
+      allow(Sources::Api::Messaging).to receive(:topics).and_return([
+        "platform.topological-inventory.operations-amazon",
+        "platform.topological-inventory.operations-openshift",
+      ]
+
+      )
     end
 
     context "post" do
@@ -373,6 +380,20 @@ RSpec.describe("v1.0 - Sources") do
                                  )
                                )))
 
+        post(check_availability_path(source.id), :headers => headers)
+
+        expect(response).to have_attributes(
+          :status      => 202,
+          :parsed_body => {}
+        )
+      end
+
+      it "success: with a source-type that topology doesn't support" do
+        source_type = SourceType.create!(:name => "vsphere", :vendor => "VMware", :product_name => "VMware vSphere")
+        attributes  = { "name" => "my_source", "source_type_id" => source_type.id.to_s }
+        source      = Source.create!(attributes.merge("tenant" => tenant))
+
+        expect(messaging_client).not_to receive(:publish_topic)
         post(check_availability_path(source.id), :headers => headers)
 
         expect(response).to have_attributes(
