36f9ac14f72affd07a35bde07972683aa38c125d
Adam Grare <agrare@redhat.com>
2020-02-27 11:52:23 -0500

    Add a before_validation to set tenant_id

--- app/models/application_authentication.rb
+++ app/models/application_authentication.rb
@@ -2,4 +2,6 @@ class ApplicationAuthentication < ApplicationRecord
   include TenancyConcern
   belongs_to :application
   belongs_to :authentication
+
+  before_validation(:on => :create) { |i| i.send("tenant=", i.authentication.send("tenant")) }
 end
