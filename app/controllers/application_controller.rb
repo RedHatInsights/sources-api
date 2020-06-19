ef6b4945777db51c22797c79d992b1b567fae96f
Madhu Kanoor <mkanoor@redhat.com>
2020-06-19 12:11:00 -0400

    Removed the exception variable which is not used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,7 +52,7 @@ class ApplicationController < ActionController::API
         else
           ActsAsTenant.without_tenant { yield }
         end
-      rescue KeyError, Insights::API::Common::IdentityError => e
+      rescue KeyError, Insights::API::Common::IdentityError
         error_document = Insights::API::Common::ErrorDocument.new.add('401', 'Unauthorized')
         render :json => error_document.to_h, :status => error_document.status
       rescue Insights::API::Common::EntitlementError
