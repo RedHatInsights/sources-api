4512ff7c90708f8fc633b10695c1d48fe9e0a049
Patrik Komanek <pkomanek@redhat.com>
2020-12-14 14:40:38 +0100

    GraphQL provide metadata with size of the result and more.

--- app/controllers/api/v1/graphql_controller.rb
+++ app/controllers/api/v1/graphql_controller.rb
@@ -3,6 +3,10 @@ require "insights/api/common/graphql"
 module Api
   module V1
     class GraphqlController < ApplicationController
+      class << self
+        attr_accessor :limit_and_offset
+      end
+
       def query
         schema_overlay = {
           "^.+$" => {
@@ -19,7 +23,61 @@ module Api
           params[:query],
           :variables => variables
         )
-        render :json => result
+
+        # metadata calculations
+        meta = {"count" => result["data"] ? result["data"]["sources"].size : 0}
+        limit_and_offset = Api::V1::GraphqlController.limit_and_offset || {}
+        limit = limit_and_offset['limit']
+        offset = limit_and_offset['offset']
+        meta["limit"]  = limit  if limit
+        meta["offset"] = offset if offset
+
+        # apply limit and offset for results
+        if meta["count"] > 0
+          result = result["data"]["sources"]
+          if offset && offset <= result.size
+            result = result[offset..-1]
+          elsif offset
+            result = []
+          end
+          result = result[0..limit - 1] if limit
+        end
+
+        render :json => {'meta' => meta, 'data' => result}
+      ensure
+        Api::V1::GraphqlController.limit_and_offset = nil
+      end
+    end
+  end
+end
+
+module GraphQL
+  module Execution
+    class Execute
+      class << self
+        def begin_query(query, _multiplex)
+          unless query.selected_operation.children[0].arguments.empty?
+            remove_offset_and_limit(query)
+          end
+
+          ExecutionFunctions.resolve_root_selection(query)
+        end
+
+        private
+
+        # Remove and save limit/offset for root element inside the query
+        def remove_offset_and_limit(query)
+          limit_and_offset = {}
+          query.selected_operation.children[0].arguments.delete_if do |arg|
+            if arg.name == 'offset' || arg.name == 'limit'
+              limit_and_offset[arg.name] = arg.value
+              true
+            else
+              false
+            end
+          end
+          Api::V1::GraphqlController.limit_and_offset = limit_and_offset
+        end
       end
     end
   end
