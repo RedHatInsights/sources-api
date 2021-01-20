eb745b57fae387bd1386f3f3169351b5f4fef27f
Jacob Lindgren <jlindgre@redhat.com>
2021-01-20 10:59:46 -0600

    add app_meta_data table + models + seeding
    Implement STI for MetaData and SuperKeyMetaData models

--- app/models/application_type.rb
+++ app/models/application_type.rb
@@ -4,4 +4,6 @@ class ApplicationType < ApplicationRecord
 
   has_many :applications
   has_many :sources, :through => :applications
+  has_many :app_meta_data, :dependent => :destroy, :class_name => "AppMetaData"
+  has_many :super_key_meta_data, :dependent => :destroy, :class_name => "SuperKeyMetaData"
 end
