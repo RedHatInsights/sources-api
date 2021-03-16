8b9067c971fc39f187abe59189d1d13cee7b8c16
Libor Pichler <lpichler@redhat.com>
2021-03-16 13:41:51 +0100

    Rename raise_event in EventConcern

--- app/models/concerns/event_concern.rb
+++ app/models/concerns/event_concern.rb
@@ -2,10 +2,10 @@ module EventConcern
   extend ActiveSupport::Concern
 
   included do
-    after_destroy :raise_event, :prepend => true
+    after_destroy :raise_event_for_destroy, :prepend => true
   end
 
-  def raise_event
+  def raise_event_for_destroy
     headers = Insights::API::Common::Request.current_forwardable
 
     Sources::Api::Events.raise_event_with_logging("#{self.class}.destroy", self.as_json, headers)
