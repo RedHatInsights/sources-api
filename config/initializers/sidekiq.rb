03a9bc386fcf9cec4b0b13ecd37d63588098e5f3
Jacob Lindgren <jlindgre@redhat.com>
2021-04-14 14:12:19 -0500

    Remove defaults from initializers since they're handled in lib class

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,14 +4,14 @@ redis_password = Sources::Api::ClowderConfig.instance["inMemoryDbPassword"]
 
 Sidekiq.configure_server do |config|
   config.redis = {
-    :url      => "redis://#{redis_host || "localhost"}:#{redis_port || 6379}/0",
+    :url      => "redis://#{redis_host}:#{redis_port}/0",
     :password => redis_password
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    :url      => "redis://#{redis_host || "localhost"}:#{redis_port || 6379}/0",
+    :url      => "redis://#{redis_host}:#{redis_port}/0",
     :password => redis_password
   }
 end
