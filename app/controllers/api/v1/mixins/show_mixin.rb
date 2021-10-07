7b7e2376b6e4ddda65f284d69cad31cce04a61d6
Jacob Lindgren <jlindgre@redhat.com>
2021-10-07 10:27:40 -0500

    limit source show to satellite sources when using system.cn auth

--- app/controllers/api/v1/mixins/show_mixin.rb
+++ app/controllers/api/v1/mixins/show_mixin.rb
@@ -5,7 +5,18 @@ module Api
         def show
           authorize(model.new)
 
-          render json: model.find(params.require(:id))
+          render json: model.find_by!(limited_access.merge!(:id => params.require(:id)))
+        end
+
+        def limited_access
+          {}.tap do |extra|
+            case model.to_s
+            when "Source"
+              if Rails.env.production? && Sources::Api::Request.current.system&.cn
+                extra["source_type_id"] = SourceType.find_by(:name => "satellite")&.id
+              end
+            end
+          end
         end
       end
     end
