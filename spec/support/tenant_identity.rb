9edaf7ce9165eb03960020915bf7a56f285e768c
Petra Cihalova <pcihalov@redhat.com>
2021-10-14 10:23:25 +0200

    Remove is_org_admin = true from identity_with_entitlements and unknown_identity

--- spec/support/tenant_identity.rb
+++ spec/support/tenant_identity.rb
@@ -8,7 +8,7 @@ module Spec
         let!(:external_tenant)  { rand(1000).to_s }
         let!(:unknown_tenant)   { rand(1000).to_s }
         let!(:identity)         { Base64.encode64({'identity' => { 'account_number' => external_tenant, 'user' => { 'is_org_admin' => true }}}.to_json) }
-        let!(:unknown_identity) { Base64.encode64({'identity' => { 'account_number' => unknown_tenant,  'user' => { 'is_org_admin' => true }}}.to_json) }
+        let!(:unknown_identity) { Base64.encode64({'identity' => { 'account_number' => unknown_tenant }}.to_json) }
         let!(:system_identity)  { Base64.encode64({'identity' => { 'account_number' => external_tenant, 'system' => {'cn' => rand(1000).to_s}}}.to_json) }
         let!(:user_identity)    { Base64.encode64({'identity' => { 'account_number' => external_tenant }}.to_json) }
 
@@ -21,7 +21,7 @@ module Spec
 
         let!(:identity_with_entitlements) do
           Base64.encode64(
-            {'identity' => { 'account_number' => external_tenant, 'user' => { 'is_org_admin' => true }}, :entitlements => entitlements}.to_json
+            {'identity' => { 'account_number' => external_tenant }, :entitlements => entitlements}.to_json
           )
         end
       end
