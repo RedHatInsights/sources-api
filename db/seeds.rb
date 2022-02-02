e63a72b4003bdc51758347c3a5df264a620fc448
Jacob Lindgren <jlindgre@redhat.com>
2022-02-02 10:48:43 -0600

    Add ability to disable seeding via ENV var, defaulting to false

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+# seeding is handled on the Go side now.
+if ENV['SKIP_SEEDING'] == "true"
+  Rails.logger.warn "Skipping seeding due to ENV"
+  exit 0
+end
+
 ApplicationType.seed
 SourceType.seed
 SuperKeyMetaData.seed
