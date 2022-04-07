7e82e86cbcfc66f87a9fb72c0b464bb91d37df1f
Jacob Lindgren <jlindgre@redhat.com>
2022-04-07 10:48:04 -0500

    Handle nil resources in authdb migration

--- db/migrate/20220405162144_migrate_auths_for_go.rb
+++ db/migrate/20220405162144_migrate_auths_for_go.rb
@@ -3,6 +3,11 @@ class MigrateAuthsForGo < ActiveRecord::Migration[5.2]
     add_column :authentications, :password_hash, :string
 
     Authentication.where.not(:password => nil).where.not(:password => "").each do |auth|
+      if auth.resource.nil?
+        Rails.logger.warn("skipping auth #{auth.id} due to resource not existing")
+        next
+      end
+
       auth.update!(:password_hash => GoEncryption.encrypt(auth.password))
     end
   end
